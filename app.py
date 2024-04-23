03862c99aa3db730e6aa4d33267a63d326a6adc1
kawashima <11011011wxy@gmail.com>
2024-04-23 23:37:52 +0900

    Temporary commit

--- app.py
+++ app.py
@@ -3,22 +3,17 @@ import openai
 
 def create_meeting_summary(openai_key, uploaded_audio):
     openai.api_key = openai_key
-    transcript = openai.Audio.transcribe("whisper-1", open(uploaded_audio, "rb"))
-    system_template = """会議の書き起こしが渡されます。
 
-この会議のサマリーをMarkdown形式で作成してください。サマリーは、以下のような形式で書いてください。
-
-- 会議の目的
-- 会議の内容
-- 会議の結果"""
+    with open(uploaded_audio.name, "rb") as audio_file:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": system_template},
             {"role": "user", "content": transcript.text}
         ]
     )
+
     response_text = completion.choices[0].message.content
     return response_text
 
